e6da2b59ab7280b861557912f946ba9c33f0f4d5
Xiaoxin <xkern@seas.upenn.edu>
2019-04-10 10:44:05 -0400

    Add AddUserInput.java

---
+++ AddUserInput.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+/*
+ * The code creates an AddUserInput class.
+ * It sums user-input numbers.
+ * When the user inputs a zero, print out the sum and then terminate.
+ */
+
+public class AddUserInput {
+	public static void main(String[] args) {
+	    float userInput;
+		float total = 0;
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Please enter a number:");
+		userInput = in.nextFloat();
+		
+		if (userInput == 0) {
+			System.out.println();
+		}
+		else {
+			do {total += userInput;
+					System.out.println("Please enter another number:");
+					userInput = in.nextFloat();
+			} while(userInput != 0);
+		    
+			System.out.println("The total of your input is " + total);
+		}
+		
+		
+		
+		in.close();
+	}
+}
+		
+	
+		
